734754148b0801f940a3f2ff0896edd0e53183b2
Nikolay Pochekuev <pochekuevn@gmail.com>
2021-04-22 19:41:45 +0400

    Storyboard excluded from project. Mapview, addButton, routeButton, restartButton addded in project via code.

---
+++ RouteCycle/Alert.swift
@@ -0,0 +1,44 @@
+//
+//  Alert.swift
+//  RouteCycle
+//
+//  Created by Nikolay Pochekuev on 22.04.2021.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func addAdressAlert(title: String, placeholder: String, completionHandler: @escaping (String) -> Void) {
+        
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField { (textfield) in
+            textfield.placeholder = placeholder
+        }
+        
+        let alertOk = UIAlertAction(title: "ok", style: .default) { (action) in
+            let textfieldText = alertController.textFields?.first
+            guard let text = textfieldText?.text else { return }
+            completionHandler(text)
+        }
+        let alertCancel = UIAlertAction(title: "cansel", style: .default) { (action) in
+            print("action")
+        }
+        
+        alertController.addAction(alertOk)
+        alertController.addAction(alertCancel)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func errorAlert(title: String, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertOkay = UIAlertAction(title: "Okay", style: .default)
+        
+        alertController.addAction(alertOkay)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
--- RouteCycle/ViewController.swift
+++ RouteCycle/ViewController.swift
@@ -6,14 +6,100 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    let addAdressButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "addButton"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let routeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "routeButton"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
+    let resetButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "resetButton"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setConstraints()
+        
+        addAdressButton.addTarget(self, action: #selector(addAdressButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(routeButtonTapped), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
     }
-
-
+    
+    @objc func addAdressButtonTapped() {
+        addAdressAlert(title: "Add Adress", placeholder: "print adress") { (text) in
+            print(text)
+        }
+    }
+    
+    @objc func routeButtonTapped() {
+        print("route tapped")
+    }
+    
+    @objc func resetButtonTapped() {
+        print("reset tapped")
+    }
+    
 }
 
+extension ViewController {
+    
+    func setConstraints() {
+        
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+        
+        mapView.addSubview(addAdressButton)
+        NSLayoutConstraint.activate([
+            addAdressButton.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            addAdressButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            addAdressButton.heightAnchor.constraint(equalToConstant: 40),
+            addAdressButton.widthAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        mapView.addSubview(routeButton)
+        NSLayoutConstraint.activate([
+            routeButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            routeButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            routeButton.heightAnchor.constraint(equalToConstant: 50),
+            routeButton.widthAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        mapView.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            resetButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            resetButton.heightAnchor.constraint(equalToConstant: 50),
+            resetButton.widthAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
